345dcf57d7209df262d2eb96c723c38b5667e2d2
ezedev <ezequiel.ivan.guzman@gmail.com>
2016-08-13 02:52:23 -0300

    boss1
    clase boss1 donde se ejecuta el comportamiento del boss
    metodo mandarsecuaces y en el update el timing del boss para aparecer,

---
+++ SpaceWar/Assets/Scripts/enemigo/Boss1.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Boss1 : MonoBehaviour {
+
+	public float vida;
+	public GameObject enemigoA;
+	// Use this for initialization
+	private float tiempoDeBajada = 3.0f;
+	private bool vulnerable= false;
+	void Start () {
+	
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (tiempoDeBajada > 0) {
+			tiempoDeBajada = tiempoDeBajada - Time.deltaTime;
+			transform.Translate (Vector3.down * 2 * Time.deltaTime);
+		}
+		//posX = (int)Random.Range (-7.60f, 7.60f);
+
+	}
+
+	public void activarSecuaces(int cantidad)
+	{
+		
+	}
+}
